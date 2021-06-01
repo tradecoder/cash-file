9b55a4d7f0c8ebebf51881ce56ceb75854df96cd
tradecoder <thetradecoder@gmail.com>
2021-06-01 16:57:08 +0600

    local update

--- README.md
+++ README.md
@@ -139,4 +139,24 @@ const firebaseConfig = {
 2. Create a file named `signup.js` inside the `screens` folder
 3. Add the below code to `signup.js`
 
+```javascript
+import React from 'react';
+import {ThemeProvider, Text, Input, Button} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+export default function Signup(){
+  return(
+    <ThemeProvider>
+      <Text h2>Please Signup</Text>
+      <Input placeholder='Name' leftIcon={{type:'font-awesome', name:'user'}}/>
+      <Input placeholder='Email Address' leftIcon={{ type: 'font-awesome', name:'envelope' }}/>
+      <Input placeholder='Password' secureTextEntry={true} leftIcon={{ type: 'font-awesome', name:'lock'}}/>  
+      <Button title="Submit"/>
+    </ThemeProvider>
+    
+  )
+}
+```
+
 
--- src/screens/signup.js
+++ src/screens/signup.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {ThemeProvider, Header, Text, Input, Button} from 'react-native-elements';
+import {ThemeProvider, Text, Input, Button} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default function Signup(){
   return(
     <ThemeProvider>
+      <Text h2>Please Signup</Text>
       <Input placeholder='Name' leftIcon={{type:'font-awesome', name:'user'}}/>
-      <Input placeholder='Email Address' leftIcon={{ type: 'font-awesome', name:'envelope' }}/>    
+      <Input placeholder='Email Address' leftIcon={{ type: 'font-awesome', name:'envelope' }}/>
+      <Input placeholder='Password' secureTextEntry={true} leftIcon={{ type: 'font-awesome', name:'lock'}}/>  
+      <Button title="Submit"/>
     </ThemeProvider>
     
   )
