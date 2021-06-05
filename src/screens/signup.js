1bf6ab1b2c4d6e703dc06f5b0b8787a958a0aecc
tradecoder <thetradecoder@gmail.com>
2021-06-05 21:29:59 +0600

    local update

--- src/screens/signup.js
+++ src/screens/signup.js
@@ -11,41 +11,49 @@ export default function Signup({nav}){
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
   const [password, setPassword] = useState("");
+  const [errorList, setErrorList] =  useState([]);
+
   function onChangeFirstName(e){
-    setFirstName(e.replace(/[^A-Za-z]/g, ''))
+    setFirstName(e.replace(/[^A-Za-z]/g, ''));
+    if(firstName.length<2){
+      setErrorList([...errorList, "Invalid first name"])
+    }
   }
   function onChangeLastName(e){
-    setLastName(e.replace(/[^A-Za-z]/g, ''))
+    setLastName(e.replace(/[^A-Za-z]/g, ''));
+    if(lastName.length<2){
+      setErrorList([...errorList, "Invalid last name"])
+    }
   }
   function onChangeEmail(e){
-    setEmail(e)
+    setEmail(e);
+    const gmailRegex = /.....@gmail.com/;
+    let mailValid = gmailRegex.test(email);
+    if(!mailValid){
+      setErrorList([...errorList, "Invalid gmail address"])
+    }
   }
   function onChangeMobile(e){    
-    setMobile(e.replace(/[^0-9]/g, ''))    
+    setMobile(e.replace(/[^0-9]/g, ''));
+    if(mobile.length !==11){
+      setErrorList([...errorList, "Invalid mobile number"])
+    }  
   }
   function onChangePassword(e){
-    setPassword(e)
-  }
-
-  function userDataValidation(){
-    let errorList = [];
-
-    if(mobile.length !==11){
-      errorList.push("Invalid mobile number")
-    } 
-    if(firstName.length<2){
-      errorList.push("Invalid first name")
-    } 
-    if(lastName.length<2){
-      errorList.push("Invalid last name")
-    }
+    setPassword(e);     
     if(password.lenth<8){
-      errorList.push("Use a strong password")
+      setErrorList([...errorList, "Use longer than 8 char password"])
     }
-    return errorList.map(e=>(<Text>{e}</Text>))
   }
 
-  function onPressSignup(e){
+  function userDataCheckPoint(){  
+    return (errorList.map(e=>{
+      return(<Text>{e}</Text>)
+    }))
+  }
+
+  function onPressSignup(e){    
+    if(errorList.length !==0 || errorList[0] !==""){
     firebase.auth()
     .createUserWithEmailAndPassword(email, password)
     .then(response=>{
@@ -59,11 +67,14 @@ export default function Signup({nav}){
       .catch(err=>alert(err))
     })
     .catch(err=>alert(err))
+  }else { 
+    return alert("Failed! Please provide correct information.")  
   }
-  
+} 
   return(
     <ThemeProvider>
       <Text h2>Please Signup</Text>
+      <Text>{userDataCheckPoint()}</Text>
       <Input placeholder='First name' value={firstName} onChangeText={onChangeFirstName} />
       <Input placeholder='Last name' value={lastName} onChangeText={onChangeLastName} />
       <Input placeholder='Email address' onChangeText={onChangeEmail} leftIcon={{ type: 'font-awesome', name:'envelope' }}/>
