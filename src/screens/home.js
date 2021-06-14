155348a12375cbfcdfa83b1cd9b177f68b23a0dc
tradecoder <thetradecoder@gmail.com>
2021-06-14 13:34:03 +0600

    update home screen

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,32 +1,79 @@
 import React, { useEffect, useState } from 'react';
-import {ThemeProvider, Text, Input, Button, Card} from 'react-native-elements';
-import {TouchableOpacity} from 'react-native';
+import { ThemeProvider, Text, Input, Button, Card, Tab } from 'react-native-elements';
+import { TouchableOpacity, View } from 'react-native';
+import { Col, Row, Grid } from "react-native-easy-grid";
 
-export default function Home(){
-    return(
-        <ThemeProvider>
+export default function Home() {
+       return (
+        <ThemeProvider theme={theme}>
             <Text> Select what you want to do</Text>
-            <Card>
-                <Card.Title>Add money</Card.Title>                       
-            </Card>
-            <Card>
-                <Card.Title>Send money</Card.Title>                         
-            </Card>
-            <Card>
-                <Card.Title>Buy a product</Card.Title>                         
-            </Card>
-            <Card>
-                <Card.Title>Sell a product</Card.Title>          
-            </Card>
-            <Card>
-                <Card.Title>Receive Loan</Card.Title>
-            </Card>
-            <Card>
-                <Card.Title>Give loan</Card.Title>
-            </Card>
-            <Card>
-                <Card.Title>Exit</Card.Title>
-            </Card>
+            <Grid>
+                <Row>
+                    <Col>
+                        <TouchableOpacity>
+                            <Card>
+                                <Card.Title>Add money</Card.Title>
+                            </Card>
+                        </TouchableOpacity>
+                    </Col>
+                    <Col>
+                        <TouchableOpacity>
+                            <Card>
+                                <Card.Title>Send money</Card.Title>
+                            </Card>
+                        </TouchableOpacity>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TouchableOpacity>
+                            <Card>
+                                <Card.Title>Buy a product</Card.Title>
+                            </Card>
+                        </TouchableOpacity>
+                    </Col>
+                    <Col>
+                        <TouchableOpacity>
+                            <Card>
+                                <Card.Title>Sell a product</Card.Title>
+                            </Card>
+                        </TouchableOpacity>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TouchableOpacity>
+                            <Card>
+                                <Card.Title>Receive loan</Card.Title>
+                            </Card>
+                        </TouchableOpacity>
+                    </Col>
+                    <Col>
+                        <TouchableOpacity>
+                            <Card>
+                                <Card.Title>Give loan</Card.Title>
+                            </Card>
+                        </TouchableOpacity>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TouchableOpacity>
+                            <Card>
+                                <Card.Title>Exit</Card.Title>
+                            </Card>
+                        </TouchableOpacity>
+                    </Col>
+                </Row>
+            </Grid>
         </ThemeProvider>
     )
+}
+const theme = {
+    Card: {
+        containerStyle: {
+            backgroundColor: "orange"
+        }
+    }
+
 }
