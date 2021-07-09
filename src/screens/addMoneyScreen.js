fe06e06a66d774ad6b235c1a2dd2ceef7168729f
tradecoder <thetradecoder@gmail.com>
2021-07-09 23:04:29 +0600

    working on addMoney

--- src/screens/addMoneyScreen.js
+++ src/screens/addMoneyScreen.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { Text, Input, Button } from 'react-native-elements';
+import React, {useState} from 'react';
+import {ThemeProvider, Text, Input, Button } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 
 
 export default function AddMoneyScreen(){
+    const [mobileAccount, setMobileAccount] = useState("");
+    const [amount, setAmount] =  useState(0);
+    const [accountType, setAccountType] = useState("");
+    function onPressAddMoney(e){
+        e.preventDefault();
 
-
-
+    } 
+    
     return(
-        <>
-        </>
+        <ThemeProvider>
+            <Text h2>Add money</Text>
+            <Input placeholder="Account Type / service name" value={accountType} onChangeText={(e)=>setAccountType(e)}/>
+            <Input placeholder="mobile account" value={mobileAccount} onChangeText={(e)=>setMobileAccount(e)}/>
+            <Input placeholder = "amount" value={amount} onChangeText={(e)=>setAmount(e)}/>
+            <Button title ="Add now" onPress={onPressAddMoney}/>
+        </ThemeProvider>
     )
+}
+
+const theme = {
+    Button:{
+        raised:true
+    }
+
 }
