b79ccd55b518a56a43556d6c7a325f59b8864841
tradecoder <thetradecoder@gmail.com>
2021-07-24 22:14:44 +0600

    update loggedin user based screen loading system

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SignupScreen, LoginScreen, HomeScreen, AddMoneyScreen, SendMoneyScreen, ReceiveLoanScreen, GiveLoanScreen, AddAccountScreen } from './src/screens/screensLib';
+import { firebase } from './src/firebase/config';
 
 // ignore yellow error 'setting a timer ...'
 import { LogBox } from 'react-native';
@@ -12,45 +13,49 @@ LogBox.ignoreLogs(['Setting a timer']);
 const Stack = createStackNavigator();
 
 export default function App() {
-  // const [user, setUser] = useState(null);
-  // const [loading, setLoading] = useState(true);
-  // if (loading) {	
-  //   return (	
-  //     <Text>Loading...</Text>	
-  //   )
-  // }
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const usersRef = firebase.firestore().collection('users');
-  //   firebase.auth().onAuthStateChanged(user => {
-  //     if (user) {
-  //       usersRef.doc(user.uid)
-  //         .get()
-  //         .then(doc => {
-  //           const userData = doc.data()
-  //           setLoading(false);
-  //           setUser(userData);
-  //         })
-  //         .catch(() => {
-  //           setLoading(false);
-  //         });
-  //     } else {
-  //       setLoading(false);
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef.doc(user.uid)
+          .get()
+          .then(doc => {
+            const userData = doc.data()         
+            setUser(userData);
+          })
+          .catch(() => {
+           setLoading(false)
+          });
+      } else {
+       setLoading(false)
+      }
+    });
+  }, []);
+  
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator>
+        {user?(
+          <>
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="AddMoney" component={AddMoneyScreen} />
+          <Stack.Screen name="SendMoney" component={SendMoneyScreen}/>
+          <Stack.Screen name="ReceiveLoan" component={ReceiveLoanScreen} />
+          <Stack.Screen name="GiveLoan" component={GiveLoanScreen} />
+          <Stack.Screen name="Account" component={AddAccountScreen} />
+          </>
+
+        ):(
+        <>
+        <Stack.Screen name="Login" component={LoginScreen} />                
         <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="AddMoney" component={AddMoneyScreen} />
-        <Stack.Screen name="SendMoney" component={SendMoneyScreen}/>
-        <Stack.Screen name="ReceiveLoan" component={ReceiveLoanScreen} />
-        <Stack.Screen name="GiveLoan" component={GiveLoanScreen} />
-        <Stack.Screen name="Account" component={AddAccountScreen} />
+        
+        </>
+         )}
       </Stack.Navigator>
     </NavigationContainer>
   );
